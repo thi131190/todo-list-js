67b973336140f22c13f24546749d2ab95a4e2f6e
thi131190 <thi131190@gmail.com>
2019-10-08 17:51:35 +0700

    complete milestone 9

---
+++ js/main.js
@@ -0,0 +1,42 @@
+let masterToDoList = [];
+
+function renderTodoList(todos) {
+	let html = '';
+	todos.map(
+		(todo, i) =>
+			(html += `
+      <li onclick='toggleDone(${i})'>
+        <span class="${todo.isDone ? 'is-done' : ''}">${todo.text}</span>
+        <a href='#' onclick='remove(${i});event.stopPropagation();'>
+          <i class="fas fa-trash-alt"></i>
+        </a>
+     	</li>`)
+	);
+	document.getElementById('todo-list').innerHTML = html;
+}
+
+function remove(index) {
+	masterToDoList.splice(index, 1);
+	renderTodoList(masterToDoList);
+}
+
+function addToDo(e) {
+	if (e.keyCode == 13) {
+		let inputValue = document.getElementById('todo').value;
+		masterToDoList.push({ text: inputValue, isDone: false });
+		document.getElementById('todo').value = '';
+		renderTodoList(masterToDoList);
+		return false;
+	}
+}
+
+function toggleDone(index) {
+	console.log(index);
+	masterToDoList[index].isDone = !masterToDoList[index].isDone;
+	renderTodoList(masterToDoList);
+}
+
+function toggleUndone() {
+	let unDoneToDoList = masterToDoList.filter(todo => todo.isDone === true);
+	document.getElementById('undone-only').checked ? renderTodoList(unDoneToDoList) : renderTodoList(masterToDoList);
+}
